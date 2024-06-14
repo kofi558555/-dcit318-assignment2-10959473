7a58d7fe8860026f1e924687228809c15d2dbc39
pkappiah014@st.ug.edu.gh <102973954+kofi558555@users.noreply.github.com>
2024-06-14 06:12:32 -0700

    Add files via upload
    2nd commit

---
+++ Shape/Shape/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    using System;
+
+    // Abstract base class
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    // Derived class Circle
+    class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    // Derived class Rectangle
+    class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double GetArea()
+        {
+            return length * width;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Circle and Rectangle
+            Circle circle = new Circle(5.0);
+            Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+            // Display areas
+            Console.WriteLine("Area of Circle: " + circle.GetArea());
+            Console.WriteLine("Area of Rectangle: " + rectangle.GetArea());
+
+            Console.ReadLine();
+        }
+    }
+}

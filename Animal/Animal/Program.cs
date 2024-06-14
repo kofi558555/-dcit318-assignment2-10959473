dcf519a5f2a43dba5a2ac85410e6633f00078176
pkappiah014@st.ug.edu.gh <102973954+kofi558555@users.noreply.github.com>
2024-06-14 05:27:09 -0700

    Add files via upload
    1st commit

---
+++ Animal/Animal/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    using System;
+
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Animal, Dog, and Cat
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Call the MakeSound() method on each instance
+            animal.MakeSound(); 
+            dog.MakeSound(); 
+            cat.MakeSound(); 
+
+            Console.ReadLine();
+        }
+    }
+
+}
